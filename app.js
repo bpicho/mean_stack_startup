1a3f1f7e340d9075abaf6ba0eeb9d727521e47d9
Bartosz Picho <bpicho@divante.pl>
2015-07-01 12:26:38 +0200

    Add some fix and features

--- app.js
+++ app.js
@@ -18,9 +18,6 @@ mongoose.connection.on('error', function() {
     console.error('MongoDB Connection Error.');
 });
 
-//////////// Zadeklarowanie autolaodera
-app.use(require('expressjs.routes.autoload')(path.join(__dirname, config.autoload.dir), config.autoload.recursive));
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -32,6 +29,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//////////// Zadeklarowanie autolaodera
+app.use(require('expressjs.routes.autoload')(path.join(__dirname, config.autoload.dir), config.autoload.recursive));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
